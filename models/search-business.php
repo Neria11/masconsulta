99a867d89e7da319699149021525d94548dbf131
Sabino Neria Cervantes <n_eria_11@hotmail.com>
2016-03-14 16:05:40 -0600

    Modelo para buscar la coincidencia con el buscador

---
+++ models/search-business.php
@@ -0,0 +1,41 @@
+<?php  
+include("connection.php");
+
+$sql = mysql_query("SELECT *
+					FROM negocios_registrados neg
+					INNER JOIN categorias_negocios cat 
+					ON neg.id_categoria = cat.id_categoria 
+					INNER JOIN logotipos_negocios log 
+					ON neg.id_negocio = log.id_negocio
+					WHERE neg.status = 1 
+					AND neg.pseudonimo like '%".$cadena."%'
+					OR neg.nombre like '%".$cadena."%'
+					OR neg.descripcion '%".$cadena."%'
+					order by rand() desc limit 0,12");
+
+
+while($row = mysql_fetch_array($sql)){
+	echo "
+			<div class='col-xs-12 col-sm-6 col-md-12 col-lg-6'>
+	          <div class='business'>
+	            <a href='negocio.php' title='".$row['categoria']."".$row['nombre']."' class='max-link'></a>
+	            <figure class='business-logo'>";
+	            	if($row['path_logotipo'] != ""){
+	            		echo "<img src='".$row['path_logotipo']."' alt='Logo ".$row['nombre']."'>";
+	            	}else{
+	            		echo "<img src='img/nologo.png' alt='Logo ".$row['nombre']."'>";
+	            	}
+	              
+	           echo "</figure>
+	            <div class='business-data'>
+	              <h4 class='name'>".$row['nombre']."</h4>
+	              <span class='nick'>@".$row['pseudonimo']."</span>
+	              <p class='category'>".$row['categoria']."</p>
+	            </div>
+	          </div>
+	        </div>
+        ";
+     
+}
+
+?>
